6af1195ebf0bc496148649fe403e025c1486c8a9
Diego123a73 <diegoplays.secundario@gmail.com>
2021-07-11 00:58:12 +0200

    new blocks

--- src/main/java/damain/neon/lights/registry/ModBlocks.java
+++ src/main/java/damain/neon/lights/registry/ModBlocks.java
@@ -4,21 +4,34 @@ import damain.neon.lights.NeonLights;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 // import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 // import net.fabricmc.yarn.constants.MiningLevels;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
-import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.block.*;
+import net.minecraft.sound.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+// import net.minecraft.util.shape.VoxelShape;
+// import net.minecraft.world.*;
 
 public class ModBlocks {
     // .breakByTool(FabricToolTags.PICKAXES, MiningLevels.STONE)
     public static final Block NEON_BLOCK = new Block(FabricBlockSettings.of(Material.GLASS).breakByHand(true)
             .hardness(0.9f).sounds(BlockSoundGroup.GLASS).luminance(15));
+
     public static final Block NEON_BAR = new Block(FabricBlockSettings.of(Material.GLASS).breakByHand(true)
-            .hardness(0.9f).sounds(BlockSoundGroup.GLASS).luminance(15));
+            .hardness(0.9f).sounds(BlockSoundGroup.GLASS).luminance(15).nonOpaque());
+
+    public static final Block PRO_BUILD = new Block(FabricBlockSettings.of(Material.GLASS).breakByHand(true)
+            .hardness(0.9f).sounds(BlockSoundGroup.GLASS).luminance(15).nonOpaque());
+
+    // @Override
+    // public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos
+    // pos, ShapeContext context) {
+    // return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 1.0f, 0.5f);
+    // }
 
     public static void registerBlocks() {
         Registry.register(Registry.BLOCK, new Identifier(NeonLights.MOD_ID, "neon_block"), NEON_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(NeonLights.MOD_ID, "neon_bar"), NEON_BAR);
+        Registry.register(Registry.BLOCK, new Identifier(NeonLights.MOD_ID, "pro_build"), PRO_BUILD);
+
     }
 }
--- src/main/java/damain/neon/lights/registry/ModItems.java
+++ src/main/java/damain/neon/lights/registry/ModItems.java
@@ -9,18 +9,24 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-    //Items
+    // Items
     public static final Item NEON_FRAGMENT = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
 
-    //Block Items
-    public static final BlockItem NEON_BLOCK = new BlockItem(ModBlocks.NEON_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
-    public static final BlockItem NEON_BAR = new BlockItem(ModBlocks.NEON_BAR, new Item.Settings().group(ItemGroup.DECORATIONS));
+    // Block Items
+    public static final BlockItem NEON_BLOCK = new BlockItem(ModBlocks.NEON_BLOCK,
+            new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
 
+    public static final BlockItem NEON_BAR = new BlockItem(ModBlocks.NEON_BAR,
+            new Item.Settings().group(ItemGroup.DECORATIONS));
+
+    public static final BlockItem PRO_BUILD = new BlockItem(ModBlocks.PRO_BUILD,
+            new Item.Settings().group(ItemGroup.DECORATIONS));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(NeonLights.MOD_ID, "neon_fragment"), NEON_FRAGMENT);
         Registry.register(Registry.ITEM, new Identifier(NeonLights.MOD_ID, "neon_block"), NEON_BLOCK);
         Registry.register(Registry.ITEM, new Identifier(NeonLights.MOD_ID, "neon_bar"), NEON_BAR);
+        Registry.register(Registry.ITEM, new Identifier(NeonLights.MOD_ID, "pro_build"), PRO_BUILD);
     }
 
 }

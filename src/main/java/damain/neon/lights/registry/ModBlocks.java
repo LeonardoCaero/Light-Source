f765be4b97984a5783bd742eaccf4f6a27e90de9
leocaero <leo@planendo.com>
2021-07-11 01:19:30 +0200

    test

--- src/main/java/damain/neon/lights/registry/ModBlocks.java
+++ src/main/java/damain/neon/lights/registry/ModBlocks.java
@@ -8,30 +8,63 @@ import net.minecraft.block.*;
 import net.minecraft.sound.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+
 // import net.minecraft.util.shape.VoxelShape;
 // import net.minecraft.world.*;
 
 public class ModBlocks {
-    // .breakByTool(FabricToolTags.PICKAXES, MiningLevels.STONE)
-    public static final Block NEON_BLOCK = new Block(FabricBlockSettings.of(Material.GLASS).breakByHand(true)
-            .hardness(0.9f).sounds(BlockSoundGroup.GLASS).luminance(15));
 
-    public static final Block NEON_BAR = new Block(FabricBlockSettings.of(Material.GLASS).breakByHand(true)
-            .hardness(0.9f).sounds(BlockSoundGroup.GLASS).luminance(15).nonOpaque());
+  // .breakByTool(FabricToolTags.PICKAXES, MiningLevels.STONE)
+  public static final Block NEON_BLOCK = new Block(
+    FabricBlockSettings
+      .of(Material.GLASS)
+      .breakByHand(true)
+      .hardness(0.9f)
+      .sounds(BlockSoundGroup.GLASS)
+      .luminance(15)
+  );
 
-    public static final Block PRO_BUILD = new Block(FabricBlockSettings.of(Material.GLASS).breakByHand(true)
-            .hardness(0.9f).sounds(BlockSoundGroup.GLASS).luminance(15).nonOpaque());
+  public static final Block NEON_BAR = new Block(
+    FabricBlockSettings
+      .of(Material.GLASS)
+      .breakByHand(true)
+      .hardness(0.9f)
+      .sounds(BlockSoundGroup.GLASS)
+      .luminance(15)
+      .nonOpaque()
+  );
 
-    // @Override
-    // public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos
-    // pos, ShapeContext context) {
-    // return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 1.0f, 0.5f);
-    // }
+  public static final Block PRO_BUILD = new Block(
+    FabricBlockSettings
+      .of(Material.GLASS)
+      .breakByHand(true)
+      .hardness(0.9f)
+      .sounds(BlockSoundGroup.GLASS)
+      .luminance(15)
+      .nonOpaque()
+  );
 
-    public static void registerBlocks() {
-        Registry.register(Registry.BLOCK, new Identifier(NeonLights.MOD_ID, "neon_block"), NEON_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLights.MOD_ID, "neon_bar"), NEON_BAR);
-        Registry.register(Registry.BLOCK, new Identifier(NeonLights.MOD_ID, "pro_build"), PRO_BUILD);
+  // @Override
+  // public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos
+  // pos, ShapeContext context) {
+  // return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 1.0f, 0.5f);
+  // }
 
-    }
+  public static void registerBlocks() {
+    Registry.register(
+      Registry.BLOCK,
+      new Identifier(NeonLights.MOD_ID, "neon_block"),
+      NEON_BLOCK
+    );
+    Registry.register(
+      Registry.BLOCK,
+      new Identifier(NeonLights.MOD_ID, "neon_bar"),
+      NEON_BAR
+    );
+    Registry.register(
+      Registry.BLOCK,
+      new Identifier(NeonLights.MOD_ID, "pro_build"),
+      PRO_BUILD
+    );
+  }
 }
